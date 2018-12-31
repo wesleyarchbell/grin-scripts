33cfc721f924c0dd6fb0e52154e8f1481c05c95e
Wesley Archbell <wesleyarchbell@gmail.com>
2018-12-31 06:57:47 +0000

    initial grin helper scripts

---
+++ run-miner.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Starting Grin Miner..."
+grin-miner/target/debug/grin-miner --floonet
---
+++ run-wallet.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pushd grin/target/release
+echo "Running wallet:listen..."
+RUST_BACKTRACE=1 ./grin --floonet wallet listen
