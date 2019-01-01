6963d49cf1264ef2f73136c9b7202878b4951eea
Wesley Archbell <wesleyarchbell@gmail.com>
2019-01-01 12:14:52 +0000

    Allow scripts to be run from outside current folder

--- run-miner.sh
+++ run-miner.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-pushd ../grin-miner
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+pushd $SCRIPT_DIR/../grin-miner
 echo "Starting grin miner..."
 target/debug/grin-miner --floonet
--- run-wallet.sh
+++ run-wallet.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-pushd ../grin/target/release
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+pushd $SCRIPT_DIR/../grin/target/release
 echo "Running wallet:listen..."
 RUST_BACKTRACE=1 ./grin --floonet wallet listen
